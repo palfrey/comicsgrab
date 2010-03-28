d2c344b157966c4db040a5c7d44de10fa2e9487b
Tom Parker <palfrey@tevp.net>
2010-03-28 19:56:59 +0100

    Add "--listme" arg rather than the "listme" argument

--- grabber.py
+++ grabber.py
@@ -11,7 +11,7 @@ from optparse import OptionParser
 
 proxy = None
 
-parser = OptionParser(usage="%prog [options] (listme/<output directory>)\n'listme' prints out an HTML formatted list of the comics specified")
+parser = OptionParser(usage="%prog [options] (<output directory>)")
 parser.add_option("-d","--debug",help="Debug level (default is 2).\n1 is random warnings (mostly old comic messages)\n2 is for when a comic page was retrieved, but the regexp couldn't be found\n3 is HTTP errors\n4 is *everything*", dest="debug", default=2, type="int")
 parser.add_option("-f", dest="strips", default="strips.def", help="Strips definition file (default is strips.def)")
 parser.add_option("-c","--cache",default="./cache", dest="cache",help="Cache directory (default is './cache')")
@@ -19,16 +19,17 @@ parser.add_option("-s","--comic",default =[],dest="comics", action="append", hel
 parser.add_option("-g","--group",default =[],dest="groups", action="append", help="Add a group to get")
 parser.add_option("-p","--proxy",default=None, dest="proxy", help="Set proxy URL")
 parser.add_option("--db", dest="db", default="comics.db")
+parser.add_option("--listme", dest="listme", default=False, action="store_true", help="Prints out an HTML formatted list of the comics specified")
 
 (opts, args) = parser.parse_args()
 
-if len(args)!=1:
-	parser.error("Need *one* arg")
+if not opts.listme and len(args)!=1:
+	parser.error("Need an output directory!")
 
 now = DateManip()
 #now.mod_days(-1)
 df = ComicsDef(opts.strips,opts.cache,debug=opts.debug,proxy=opts.proxy, db=opts.db)
-if args[0] == "listme":
+if opts.listme:
 	print "<ul>"
 	for x in df.get_strips(opts.comics,opts.groups,now=now):
 		print "<li><a href=\""+x.entries["homepage"]+"\">"+x.entries["name"]+"</a></li>"
