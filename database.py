2683c47578a14d22405aeae35b2596abc990e901
Tom Parker <palfrey@tevp.net>
2010-04-11 20:29:18 +0100

    Add MySQL support to database class

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 import sqlite3 as sqlite
+import MySQLdb
 from strips_pb2 import Class,Strip,Subsection,User
 
 class NoSuchStrip(Exception):
@@ -11,39 +12,35 @@ class NoSuchClass(Exception):
 	pass
 
 class Database:
-	pass
+	def list_users(self):
+		return self._list("users")
 
-class Sqlite(Database):
-	def __init__(self,db="comics.db"):
-		self._con = sqlite.connect(db)
-		self._cur = self._con.cursor()
-		self._cur.execute("select name from sqlite_master where type='table' and name='strips'")
-		if len(self._cur.fetchall())==0:
-			self._setup(("users","strips","classes"))
+	def list_classes(self):
+		return self._list("classes")
+
+	def list_strips(self):
+		return self._list("strips")
+
+	def list_user_strips(self, user):
+		return list(user.include)
 	
-	def _setup(self, tables):
-		for t in tables:
-			if t == "users":
-				self._cur.execute("create table users (name text primary key, pb blob)")
-			elif t == "strips":
-				self._cur.execute("create table strips (name text primary key, desc text,homepage text, pb blob)")
-			elif t == "classes":
-				self._cur.execute("create table classes (name text primary key, desc text,pb blob)")
-			else:
-				raise Exception, t
-		self._con.commit()
+	def clear_strips(self):
+		self._clear_tables(("strips","classes"))
+		
+	def clear_users(self):
+		self._clear_tables(("users",))
 
 	def add_section(self, s):
 		if isinstance(s,Strip):
-			self._cur.execute("insert into strips values (?,?,?,?)",(s.name,s.desc,s.homepage,sqlite.Binary(s.SerializeToString())))
+			self._cur.execute("insert into strips values ("+self.replace_str+","+self.replace_str+","+self.replace_str+","+self.replace_str+")",(s.name,s.desc,s.homepage,sqlite.Binary(s.SerializeToString())))
 			self._con.commit()
 		elif isinstance(s,User):
 			data = (s.name,sqlite.Binary(s.SerializeToString()))
-			self._cur.execute("insert into users values (?,?)",data)
+			self._cur.execute("insert into users values ("+self.replace_str+","+self.replace_str+")",data)
 			self._con.commit()
 		
 		elif isinstance(s,Class):
-			self._cur.execute("insert into classes values (?,?,?)",(s.name,s.desc,sqlite.Binary(s.SerializeToString())))
+			self._cur.execute("insert into classes values ("+self.replace_str+","+self.replace_str+","+self.replace_str+")",(s.name,s.desc,sqlite.Binary(s.SerializeToString())))
 			self._con.commit()
 		else:
 			raise Exception,(s,type(s))
@@ -62,28 +59,10 @@ class Sqlite(Database):
 			self._clear_table(table)
 		self._con.commit()
 		self._setup(tables)
-
-	def clear_strips(self):
-		self._clear_tables(("strips","classes"))
-		
-	def clear_users(self):
-		self._clear_tables(("users",))
 	
 	def _list(self, table):
 		self._cur.execute("select name from %s order by name"%table)
 		return [x[0] for x in self._cur.fetchall()]
-
-	def list_users(self):
-		return self._list("users")
-
-	def list_classes(self):
-		return self._list("classes")
-
-	def list_strips(self):
-		return self._list("strips")
-
-	def list_user_strips(self, user):
-		return list(user.include)
 
 	def get_strip(self, strip):
 		self._cur.execute("select pb from strips where name='%s'"%strip)
@@ -104,7 +83,7 @@ class Sqlite(Database):
 		return c
 
 	def get_user(self, user):
-		self._cur.execute("select pb from users where name=?",(user,))
+		self._cur.execute("select pb from users where name="+self.replace_str+"",(user,))
 		f = self._cur.fetchall()
 		if len(f)!=1:
 			print f
@@ -112,3 +91,50 @@ class Sqlite(Database):
 		u = User()
 		u.ParseFromString(f[0][0])
 		return u
+
+class MySQL(Database):
+	replace_str = "%s"
+
+	def __init__(self,database="comics"):
+		self._con = MySQLdb.connect(user="comics",passwd="comics",db=database)
+		self._cur = self._con.cursor()
+		for table in ("users","strips","classes"):
+			self._cur.execute("show tables like '%s'"%table)
+			if len(self._cur.fetchall())==0:
+				self._setup((table,))
+
+	def _setup(self, tables):
+		for t in tables:
+			if t == "users":
+				self._cur.execute("create table users (name varchar(100) primary key, pb blob)")
+			elif t == "strips":
+				self._cur.execute("create table strips (name varchar(100) primary key, description varchar(200), homepage varchar(200), pb blob)")
+			elif t == "classes":
+				self._cur.execute("create table classes (name varchar(100) primary key, description varchar(200), pb blob)")
+			else:
+				raise Exception, t
+		self._con.commit()
+
+class Sqlite(Database):
+	replace_str = "?"
+
+	def __init__(self,db="comics.db"):
+		self._con = sqlite.connect(db)
+		self._cur = self._con.cursor()
+		self._cur.execute("select name from sqlite_master where type='table' and name='strips'")
+		if len(self._cur.fetchall())==0:
+			self._setup(("users","strips","classes"))
+	
+	def _setup(self, tables):
+		for t in tables:
+			if t == "users":
+				self._cur.execute("create table users (name text primary key, pb blob)")
+			elif t == "strips":
+				self._cur.execute("create table strips (name text primary key, desc text,homepage text, pb blob)")
+			elif t == "classes":
+				self._cur.execute("create table classes (name text primary key, desc text,pb blob)")
+			else:
+				raise Exception, t
+		self._con.commit()
+
+
