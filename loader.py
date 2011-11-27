95af9826197e6bc6e19e437dde3f2c6f8ef89442
Tom Parker <palfrey@tevp.net>
2011-11-27 19:30:19 +0000

    Refactor loader.py for use in website

--- loader.py
+++ loader.py
@@ -2,89 +2,94 @@ from google.protobuf.internal.containers import BaseContainer
 from strips_pb2 import Class,User,Strip,Subsection,_TYPE
 from re import split
 
-from optparse import OptionParser
+def load_data(db, user, deffile):
+	if user:
+		db.clear_users()
+	else:
+		db.clear_strips()
 
-parser = OptionParser()
-parser.add_option("-d","--db",dest="database",default="comics.db",help="Set database to use")
-parser.add_option("-u","--user",dest="user",default=False,action="store_true",help="Load user database")
-parser.add_option("-m","--module",dest="db_module",default="Sqlite",help="Specify database module")
-(opts,args) = parser.parse_args()
+	infile = open(deffile)
+	storage = []
 
-if len(args)!=1:
-	parser.error("Need a definitions file!")
+	for lineno,line in enumerate(infile):
+		while line.find("#")<>-1:
+			line = line[:line.find("#")-1]
+		line = line.strip()
+		if line == "":
+			continue
 
-globals()['Database'] = getattr(__import__('database',globals(),locals(),[opts.db_module]),opts.db_module)
-
-if opts.db_module == "Sqlite":
-	db = Database(opts.database)
-else:
-	db = Database()
-if opts.user:
-	db.clear_users()
-else:
-	db.clear_strips()
-
-deffile = args[0]
-infile = open(deffile)
-storage = []
-for lineno,line in enumerate(infile):
-	while line.find("#")<>-1:
-		line = line[:line.find("#")-1]
-	line = line.strip()
-	if line == "":
-		continue
-
-	try:
-		data = split('[ \t]',line,1)
-		#print data 
-		if storage == []:
-			if data[0] == "user":
-				storage.append(User())
-			elif data[0] == "class":
-				storage.append(Class())
-			elif data[0] == "strip":
-				storage.append(Strip())
-			else:
-				raise Exception,"Unknown block type '%s'"%data[0]
-			storage[-1].name = data[1]
-		elif data[0] == 'end':
-			assert len(storage) == 1,storage
-			db.add_section(storage[0])
-			storage = []
-		else:
-			if data[0] == "subbeg":
-				storage.append(storage[-1].subs.add())
-			elif data[0] == "name":
-				storage[-1].desc = data[1]
-			elif data[0] == "subend":
-				assert len(storage)>1
-				storage = storage[:-1]
-			elif data[0] == "include":
-				storage[-1].include.extend(data[1].split(" "))
-			else:
-				if data[0][0] == "$":
-					data[0] = "_var_"+data[0][1:]
-				attr = getattr(storage[-1],data[0])
-				if isinstance(attr,BaseContainer): # composite type, use append
-					attr.append(data[1])
+		try:
+			data = split('[ \t]',line,1)
+			#print data 
+			if storage == []:
+				if data[0] == "user":
+					storage.append(User())
+				elif data[0] == "class":
+					storage.append(Class())
+				elif data[0] == "strip":
+					storage.append(Strip())
 				else:
-					if len(data)==1:
-						data.append("")
-					if data[0] in ("index",):
-						data[1] = int(data[1])
-					elif data[0] in ("zoom",):
-						data[1] = float(data[1])
-					elif data[0] in ("enabled","noperl"):
-						if data[1] in ("true","True"):
-							data[1] = True
-						else:
-							data[1] = False
-					elif data[0] == "type":
-						data[1] = _TYPE.values_by_name[data[1]].number
+					raise Exception,"Unknown block type '%s'"%data[0]
+				storage[-1].name = data[1]
+			elif data[0] == 'end':
+				assert len(storage) == 1,storage
+				db.add_section(storage[0])
+				storage = []
+			else:
+				if data[0] == "subbeg":
+					storage.append(storage[-1].subs.add())
+				elif data[0] == "name":
+					storage[-1].desc = data[1]
+				elif data[0] == "subend":
+					assert len(storage)>1
+					storage = storage[:-1]
+				elif data[0] == "include":
+					storage[-1].include.extend(data[1].split(" "))
+				else:
+					if data[0][0] == "$":
+						data[0] = "_var_"+data[0][1:]
+					attr = getattr(storage[-1],data[0])
+					if isinstance(attr,BaseContainer): # composite type, use append
+						attr.append(data[1])
 					else:
-						pass
-					setattr(storage[-1],data[0],data[1])
-	except:
-		print "error at line number %d in file %s"%(lineno+1,deffile)
-		raise
-infile.close()
+						if len(data)==1:
+							data.append("")
+						if data[0] in ("index",):
+							data[1] = int(data[1])
+						elif data[0] in ("zoom",):
+							data[1] = float(data[1])
+						elif data[0] in ("enabled","noperl"):
+							if data[1] in ("true","True"):
+								data[1] = True
+							else:
+								data[1] = False
+						elif data[0] == "type":
+							data[1] = _TYPE.values_by_name[data[1]].number
+						else:
+							pass
+						setattr(storage[-1],data[0],data[1])
+		except:
+			print "error at line number %d in file %s"%(lineno+1,deffile)
+			raise
+	return
+	infile.close()
+
+if __name__ == "__main__":
+	from optparse import OptionParser
+
+	parser = OptionParser()
+	parser.add_option("-d","--db",dest="database",default="comics.db",help="Set database to use")
+	parser.add_option("-u","--user",dest="user",default=False,action="store_true",help="Load user database")
+	parser.add_option("-m","--module",dest="db_module",default="Sqlite",help="Specify database module")
+	(opts,args) = parser.parse_args()
+
+	if len(args)!=1:
+		parser.error("Need a definitions file!")
+
+	globals()['Database'] = getattr(__import__('database',globals(),locals(),[opts.db_module]),opts.db_module)
+
+	if opts.db_module == "Sqlite":
+		db = Database(opts.database)
+	else:
+		db = Database()
+	load_data(db, opts.user, args[0])
