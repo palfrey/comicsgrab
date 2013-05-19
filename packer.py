247fad57bed3c2683c5bf44ff9ca690a7afd621d
Tom Parker <palfrey@tevp.net>
2013-05-19 22:42:45 +0100

    Packer.py: Helper script to aid bundling archived stuff into year CBZs

---
+++ packer.py
@@ -0,0 +1,21 @@
+from sys import argv
+from os import listdir, system
+from os.path import exists
+
+folder = argv[1]
+
+years = []
+
+for f in sorted(listdir(folder)):
+	year = int(f[:4])
+	if year not in years:
+		years.append(year)
+
+print years
+
+for year in years:
+	fname = "%s-%s.cbz"%(folder, year)
+	if not exists(fname):
+		cmd = "zip %s %s/%s*"%(fname,folder, year)
+		print cmd
+		system(cmd)
