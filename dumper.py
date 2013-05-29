d447afecb2f07448667eaa4b79361e371d40a6f8
Tom Parker <palfrey@tevp.net>
2013-05-29 23:37:14 +0100

    Add one-file option to dumper

--- dumper.py
+++ dumper.py
@@ -49,25 +49,27 @@ def print_pb_indent(fp, pb,indent="\t"):
 			name = fd.name
 		print >>fp, "%s%s %s"%(indent,name,str(val))
 
+def print_pb_internal(fp, pb):
+	print >> fp, "%s %s"%(pb.__class__.__name__.lower(),pb.name)
+	print_pb_indent(fp, pb)
+	print >> fp, "end"
+
 def print_pb(folder, pb):
 	path = join(folder, pb.name)
 	if not exists(folder):
 		mkdir(folder)
 	with open(path, "wb") as fp:
-		print >> fp, "%s %s"%(pb.__class__.__name__.lower(),pb.name)
-		print_pb_indent(fp, pb)
-		print >> fp, "end"
+		print_pb_internal(fp, pb)
 
 parser = OptionParser()
 parser.add_option("-u","--user",dest="user",default=False,action="store_true",help="Dump user database")
 parser.add_option("-d","--db",dest="db",default="comics.db",help="Set database to use")
 parser.add_option("-m","--module",dest="db_module",default="Sqlite",help="Specify database module")
+parser.add_option("--one-file", dest="one_file", default=False, action="store_true", help="Dump to file, not folder")
 (opts,args) = parser.parse_args()
 
 if len(args) != 1:
 	parser.error("Need folder")
-
-folder = args[0]
 
 globals()['Database'] = getattr(__import__('database',globals(),locals(),[opts.db_module],-1),opts.db_module)
 
@@ -79,7 +81,16 @@ else:
 if opts.user:
 	for user in db.list_users():
 		print_pb("user",db.get_user(user))
+elif opts.one_file:
+	with open(args[0], "wb") as fp:
+		for cl in db.list_classes():
+			print_pb_internal(fp,db.get_class(cl))
+			print >>fp, ""
+		for s in db.list_strips():
+			print_pb_internal(fp,db.get_strip(s))
+			print >>fp, ""
 else:
+	folder = args[0]
 	if not exists(folder):
 		mkdir(folder)
 	for cl in db.list_classes():
