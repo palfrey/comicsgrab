4bb6889d80afe75a20dc34f502173fa38670c297
Tom Parker <palfrey@tevp.net>
2013-05-29 23:35:32 +0100

    Claim to be from Chromium so we get more working pages

--- urlcache.py
+++ urlcache.py
@@ -114,7 +114,7 @@ class URLCache:
 			try:
 				ut = URLTimeout() 
 				ut.setTimeout(120)
-				data = ut.get(url,ref=ref,proxy=self.proxy)
+				data = ut.get(url,ref=ref,proxy=self.proxy, headers = {"User-Agent":"Mozilla/5.0 (X11; Linux i686) AppleWebKit/537.4 (KHTML, like Gecko) Chrome/22.0.1229.94 Safari/537.4"})
 			except urllib2.HTTPError,err:
 				if err.code==404:
 					return None
