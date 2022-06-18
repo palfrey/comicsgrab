6127854a42c96a4cf9765bcc4665a35a13c3325c
Tom Parker-Shemilt <palfrey@tevp.net>
2022-06-18 14:50:49 +0100

    Django 4.x support

--- django/urls.py
+++ django/urls.py
@@ -1,13 +1,13 @@
-from django.conf.urls import url
+from django.urls import re_path
 from . import views
 from django.views.static import serve
 from django.conf import settings
 
 urlpatterns = [
-    url(r'^$', views.index),
-    url(r'^(?P<path>[0-9]{4}-[0-9]{2}-[0-9]{2}/[^/]+)$', serve, {
+    re_path(r'^$', views.index),
+    re_path(r'^(?P<path>[0-9]{4}-[0-9]{2}-[0-9]{2}/[^/]+)$', serve, {
             'document_root': settings.COMICS_DIR,
         }),
-    url(r'^user/(?P<user>.+)', views.update_strips_for_user, name="update_strips_for_user"),
-    url(r'^(?P<strip>.+)', views.update_strip, name="update_strip"),
+    re_path(r'^user/(?P<user>.+)', views.update_strips_for_user, name="update_strips_for_user"),
+    re_path(r'^(?P<strip>.+)', views.update_strip, name="update_strip"),
 ]
